8c55ab3d3952f197a389de6966ac78faf8995eb7
Pablo <pablodiegodemuner@gmail.com>
2022-01-16 13:20:40 -0400

    Projeto Iniciado

---
+++ src/main/java/com/projetospringbatchapi/GeraFaturaCartaoBatchApplication.java
@@ -0,0 +1,13 @@
+package com.projetospringbatchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeraFaturaCartaoBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeraFaturaCartaoBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetospringbatchapi/GeraFaturaCartaoBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetospringbatchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeraFaturaCartaoBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
